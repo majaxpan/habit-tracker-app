d01a432837a29e8269df0d8ced346f45f5558153
Maja Panoska <panoskamaja@outlook.com>
2026-05-21 22:09:48 +0200

    Add measurable habit logging and habit creation functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  //habits + todays entries
   const [habits, setHabits] = useState([]);
   const [entries, setEntries] = useState([]);
+  const [inputValues, setInputValues] = useState({});
+
+  //add habit form
+  const [name, setName] = useState("");
+  const [type, setType] = useState("boolean");
+  const [unit, setUnit] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/habits")
@@ -27,7 +34,59 @@ function App() {
         <h1>Habit tracker</h1>
       </div>
 
-      <button
+      <div style={{ marginBottom: "20px" }}>
+        <h3>Add Habit</h3>
+
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <option value="boolean">non-measurable</option>
+          <option value="measurable">measurable</option>
+        </select>
+
+        <input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        {type === "measurable" && (
+          <input
+            placeholder="unit (e.g. minutes)"
+            value={unit}
+            onChange={(e) => setUnit(e.target.value)}
+          />
+        )}
+
+        <button
+          onClick={async () => {
+            if (!name.trim()) return;
+
+            if (type === "measurable" && !unit.trim()) return;
+
+            const res = await fetch("http://localhost:5000/habits", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                name,
+                type,
+                unit: type === "boolean" ? null : unit,
+              }),
+            });
+
+            const newHabit = await res.json();
+
+            setHabits((prev) => [...prev, newHabit]);
+
+            //clear the form
+            setName("");
+            setUnit("");
+            setType("boolean");
+          }}
+        >
+          Add
+        </button>
+      </div>
+
+      {/* <button
         onClick={async () => {
           await fetch("http://localhost:5000/entries", {
             method: "POST",
@@ -46,7 +105,7 @@ function App() {
         }}
       >
         Log Meditation (test)
-      </button>
+      </button> */}
 
       <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
         <h4>Habit</h4>
@@ -63,11 +122,17 @@ function App() {
           >
             <div>{habit.name}</div>
             <div>
-              {entry ? entry.value : "-"}
+              {entry
+                ? habit.type === "measurable"
+                  ? `${entry.value} ${habit.unit}`
+                  : entry.value
+                : "-"}
 
               {habit.type === "boolean" && (
                 <button
                   onClick={async () => {
+                    //const newValue = entry?.value === "true" ? "-" : "true";
+
                     await fetch("http://localhost:5000/entries", {
                       method: "POST",
                       headers: {
@@ -75,7 +140,7 @@ function App() {
                       },
                       body: JSON.stringify({
                         habitId: habit.id,
-                        value: "true", // for now hardcoded
+                        value: "true", //newValue,
                       }),
                     });
 
@@ -88,6 +153,43 @@ function App() {
                 >
                   Log
                 </button>
+              )}
+              {habit.type === "measurable" && (
+                <div>
+                  <input
+                    type="number"
+                    value={inputValues[habit.id] || ""}
+                    onChange={(e) => {
+                      setInputValues({
+                        ...inputValues,
+                        [habit.id]: e.target.value,
+                      });
+                    }}
+                  />
+                  <button
+                    onClick={async () => {
+                      await fetch("http://localhost:5000/entries", {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                          habitId: habit.id,
+                          value: inputValues[habit.id],
+                        }),
+                      });
+
+                      const res = await fetch(
+                        "http://localhost:5000/entries/today",
+                      );
+
+                      const data = await res.json();
+                      setEntries(data);
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
               )}
             </div>
           </div>
